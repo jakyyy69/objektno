4d6666b29315e3425f9e76ec14ea2da4c2297248
jakyyy69 <prijavlanjenasranje@gmail.com>
2018-10-22 19:50:15 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace objektno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uraLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uraLabel
+            // 
+            this.uraLabel.AutoSize = true;
+            this.uraLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.uraLabel.Location = new System.Drawing.Point(111, 69);
+            this.uraLabel.Name = "uraLabel";
+            this.uraLabel.Size = new System.Drawing.Size(76, 25);
+            this.uraLabel.TabIndex = 0;
+            this.uraLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 188);
+            this.Controls.Add(this.uraLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uraLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace objektno
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Vozilo
+        {
+            public int stKoles;
+            public string imeLastnika;
+            public string priimekLastnika;
+
+            public Vozilo()
+            {
+                imeLastnika = "joze";
+                priimekLastnika = "gaj";
+                stKoles = 0;
+            }
+
+            public Vozilo(int gume)
+            {
+                imeLastnika = "David";
+                priimekLastnika = "Škulj";
+                stKoles = gume;
+            }
+
+            public Vozilo(int gume, string jme, string prijmek)
+            {
+                imeLastnika = jme;
+                priimekLastnika = prijmek;
+                stKoles = gume;
+            }
+
+            void zameji(string jme, string prijmek)
+            {
+                imeLastnika = jme;
+                priimekLastnika = prijmek;
+            }
+
+            string Izpisi()
+            {
+                return imeLastnika + " " + priimekLastnika;
+            }
+
+            int vVisjoBrzino(int brzina)
+            {
+                return brzina = brzina + 1;
+            }
+
+            int vNizjoBrzino(int brzina)
+            {
+                return brzina = brzina - 1;
+            }
+
+            string potrobi(string potrobi)
+            {
+                return potrobi;
+            }
+        }
+
+        public class Avto : Vozilo
+        {
+            char masina;
+            int stVrat;
+            int ccm, brzine;
+        }
+
+        public class Kolo : Vozilo
+        {
+            int brzine;
+            bool blatnik;
+            char spol;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer tajmer = new Timer();
+            tajmer.Interval = 10;
+            tajmer.Tick += new EventHandler(timer_tick);
+            tajmer.Start();
+            uraLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            this.Hide();
+            form.Show();
+            Invalidate();
+        }
+    }
+}
